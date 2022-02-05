d8b01b5a543315b371266e7838f4ca8573f14845
Danilo Silva <danilo.silva@msn.com>
2022-02-05 17:15:32 -0300

    Aula sobre SqlConnection

--- BaltaDataAccess/Program.cs
+++ BaltaDataAccess/Program.cs
@@ -10,7 +10,17 @@ namespace BaltaDataAccess
             // Servidor do Banco de Dados + Porta + Banco de Dados + Usuário + Senha
             const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
 
-            // Microsoft.Data.SqlClient (NUGET)
+            var connection = new SqlConnection();
+            connection.Open();
+            // INSERT
+            // UPDATE
+            connection.Close();
+            connection.Dispose();
+
+            using (var connectionUsing = new SqlConnection())
+            {
+                Console.WriteLine("Conectado");
+            }
         }
     }
 }
