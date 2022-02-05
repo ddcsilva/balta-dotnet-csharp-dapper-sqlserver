71397952b1ed0e0f1439cb2eb0eb0caea939cf19
Danilo Silva <danilo.silva@msn.com>
2022-02-05 17:31:21 -0300

    Aula sobre SqlCommand e SqlDataReader + TrustServerCertificate no ConnectionString

--- BaltaDataAccess/Program.cs
+++ BaltaDataAccess/Program.cs
@@ -7,19 +7,27 @@ namespace BaltaDataAccess
     {
         static void Main(string[] args)
         {
-            // Servidor do Banco de Dados + Porta + Banco de Dados + Usuário + Senha
-            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
+            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True";
 
-            var connection = new SqlConnection();
-            connection.Open();
-            // INSERT
-            // UPDATE
-            connection.Close();
-            connection.Dispose();
-
-            using (var connectionUsing = new SqlConnection())
+            using (var connection = new SqlConnection(connectionString))
             {
                 Console.WriteLine("Conectado");
+
+                connection.Open();
+
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+                    var reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                    }
+                }
             }
         }
     }
