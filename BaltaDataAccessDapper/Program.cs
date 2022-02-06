fdf8eedf5337652609ef122e6c274416fa076b42
Danilo Silva <danilo.silva@msn.com>
2022-02-06 02:11:47 -0300

    Aula sobre Instação do Dapper

---
+++ BaltaDataAccessDapper/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using Dapper;
+
+namespace BaltaDataAccessDapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+
+            }
+        }
+    }
+}
