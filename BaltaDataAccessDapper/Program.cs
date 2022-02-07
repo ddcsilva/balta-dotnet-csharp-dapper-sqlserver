df7d9da09ece49397c9a633f4718b8797334c8b7
Danilo Silva <danilo.silva@msn.com>
2022-02-06 21:25:13 -0300

    Implementando CREATE e UPDATE por Dapper

--- BaltaDataAccessDapper/Program.cs
+++ BaltaDataAccessDapper/Program.cs
@@ -11,18 +11,61 @@ namespace BaltaDataAccessDapper
         {
             const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True";
 
-            var category = new Category();
-            var insertSql = "INSERT INTO [Category] VALUES (id, title, url, summary, order, description, featured)";
-
             using (var connection = new SqlConnection(connectionString))
             {
-                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-
-                foreach (var category in categories)
-                {
-                    Console.WriteLine($"{category.Id} - {category.Title}");
-                }
+                UpdateCategory(connection);
+                ListCategories(connection);
+                // CreateCategory(connection);
             }
+        }
+
+        static void ListCategories(SqlConnection connection)
+        {
+            var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+
+            foreach (var item in categories)
+            {
+                Console.WriteLine($"{item.Id} - {item.Title}");
+            }
+        }
+
+        static void CreateCategory(SqlConnection connection)
+        {
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Summary = "AWS Cloud";
+            category.Order = 8;
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Featured = false;
+
+            var insertSql = "INSERT INTO [Category] VALUES (@id, @title, @url, @summary, @order, @description, @featured)";
+
+            var rows = connection.Execute(insertSql, new
+            {
+                category.Id,
+                category.Title,
+                category.Url,
+                category.Summary,
+                category.Order,
+                category.Description,
+                category.Featured
+            });
+
+            Console.WriteLine($"{rows} linhas inseridas");
+        }
+
+        static void UpdateCategory(SqlConnection connection)
+        {
+            var updateQuery = "UPDATE [Category] SET [Title] = @title WHERE [Id] = @id";
+            var rows = connection.Execute(updateQuery, new
+            {
+                id = new Guid("9f3407aa-11ae-4621-a2ef-2028b85507c4"),
+                title = "Frontend 2022"
+            });
+
+            Console.WriteLine($"{rows} registros atualizados");
         }
     }
 }
